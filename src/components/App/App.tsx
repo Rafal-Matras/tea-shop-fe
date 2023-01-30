077d22b5b539415ea37022505031844fa74d02c1
Rafał Matraś <rafalmatras@gmail.com>
2023-01-30 16:07:02 +0100

    create a simple AppContext for App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,14 +3,20 @@ import { Header } from '../Header/Header';
 import { Main } from '../Main/Main';
 
 import style from './App.module.css';
+import { AppContext } from '../../context/AppContext';
+import { useState } from 'react';
 
 export const App = () => {
 
+  const [activeUser, setActiveUser] = useState<boolean>(true);
+
   return (
+    <AppContext.Provider value={{activeUser, setActiveUser}}>
       <main className={style.container}>
         <Header/>
         <Main/>
         <Footer/>
       </main>
+    </AppContext.Provider>
   );
 };
