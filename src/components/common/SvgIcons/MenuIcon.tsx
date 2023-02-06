c0bee95437a9db53a71e84a2058a724a26f44ec3
Rafał Matraś <rafalmatras@gmail.com>
2023-02-06 12:26:06 +0100

    icon components created.

---
+++ src/components/common/SvgIcons/MenuIcon.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from 'react';
+
+export const MenuIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 24 24"
+      fill="#7ab744"
+      {...props}
+    >
+      <path
+        d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"
+      >
+      </path>
+    </svg>
+  );
+};
