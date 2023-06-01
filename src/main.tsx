3a0adf5d524eebf9edac28e3c940273d38b18b34
Rafał Matraś <rafalmatras@gmail.com>
2023-06-01 17:06:15 +0200

    wrapping the app component with the basket context

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,15 @@ import { UserContextProvider } from './context/UserContext';
 import { App } from './components/App/App';
 
 import './index.css';
+import { BasketContextProvider } from './context/BasketContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserContextProvider>
-        <App/>
+        <BasketContextProvider>
+          <App/>
+        </BasketContextProvider>
       </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
